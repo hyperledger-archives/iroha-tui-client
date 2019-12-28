795eb05c9d5fe86648c55732fead120336ce9f79
Mikhail Boldyrev <miboldyrev@gmail.com>
2019-12-28 08:35:03 +0300

    fixup! added genesis block generation
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- iroha_tui/proto/genesis_block_creator.py
+++ iroha_tui/proto/genesis_block_creator.py
@@ -1,7 +1,6 @@
 from typing import TYPE_CHECKING, Iterable
 
-from datetime import datetime
-
+from iroha import Iroha
 from iroha.block_pb2 import Block
 
 if TYPE_CHECKING:
@@ -16,7 +15,7 @@ def create_genesis_block(txs: Iterable['Transaction']) -> Block:
     block = Block()
     for tx in txs:
         block.block_v1.payload.transactions.append(tx)
-    block.block_v1.payload.created_time = int(datetime.now().timestamp())
+    block.block_v1.payload.created_time = Iroha.now()
     block.block_v1.payload.height = 1
     block.block_v1.payload.prev_block_hash = b'0' * 64
     block.block_v1.payload.tx_number = len(block.block_v1.payload.transactions)
